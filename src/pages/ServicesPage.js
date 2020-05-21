c7333ab1fb8458620c748d7a008d5f8542eee29b
irabi kassim <irabikassim01@gmail.com>
2020-05-21 22:21:09 +0300

    finalizing service page

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import im1 from '../Images/bg10.jpg'
+import img2 from '../Images/bg17.jpg'
 
 class ServicesPage extends Component {
     render() {
@@ -144,6 +146,95 @@ class ServicesPage extends Component {
 
                     </div>
                 </div>
+
+
+
+                <div className="services_maintenance_quotation">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="content">
+                                    <h1>We have skilled and competent team to provide dynamic technical maintenance support on your computer systems.</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="services_maintenance">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="content">
+                                    <img src={im1} alt="service_maintenance" className="img-fluid"/>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="content_desc w3-card">
+                                    <h1>Software Maintenance service</h1>
+                                    <ul>
+                                        <li>Software repair fixing bugs</li>
+                                        <li>Software upgrade</li>
+                                        <li>Defragmentation of hard disk</li>
+                                        <li>Software capability enhancement</li>
+                                        <li>Performance improvement</li>
+                                    </ul>
+                                </div>
+                                <div className="content_desc_2 w3-card">
+                                    <h1>Hardware Maintenance service</h1>
+                                    <ul>
+                                        <li>Printer maintenance</li>
+                                        <li>Photocopy maintenance</li>
+                                        <li>computer maintenance</li>
+                                        <li>Hardware cleanup</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="service_server_quote">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="content">
+                                    <h1>We offer server and network maintenance services to guarantee reliability and availability of your organization critical services by reducing unexpected downtimes.</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="service_server">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-5">
+                                <div className="content_img">
+                                    <img src={img2} alt="server" className="img-fluid"/>
+                                </div>
+                            </div>
+                            <div className="col-md-7">
+                                <div className="content_desc w3-card">
+                                    <h1>Server and network services are</h1>
+                                    <ul>
+                                        <li>Patch management</li>
+                                        <li>System backup and archiving</li>
+                                        <li>Access management</li>
+                                        <li>Network performance monitoring and improving</li>
+                                        <li>Network capacity management</li>
+                                        <li>Network devices replacement and configuration optimization</li>
+                                        <li>Server and storage capacity and performance maintenance</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
 
             </div>
         )
